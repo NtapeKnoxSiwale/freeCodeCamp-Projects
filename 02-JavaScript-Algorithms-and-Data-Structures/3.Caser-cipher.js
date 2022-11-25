12ec464de708b7a11fe0d3c48e5905f0206e35cb
NtapeKnoxSiwale <ntape.k.siwale@gmail.com>
2022-11-25 16:24:01 +0200

    JS certifcation.

---
+++ 02-JavaScript-Algorithms-and-Data-Structures/3.Caser-cipher.js
@@ -0,0 +1,18 @@
+function encrypt(str) {
+  let alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let splits = str.split("");
+
+  for (let i = 0; i < str.length; i++) {
+    if (/\W/.test(splits[i])) continue;
+
+    if (alphabets.indexOf(str[i]) + 13 >= 26) {
+      splits[i] = alphabets[alphabets.indexOf(str[i]) + 13 - 26];
+    } else {
+      splits[i] = alphabets[alphabets.indexOf(str[i]) + 13];
+    }
+  }
+
+  return splits.join("");
+}
+
+encrypt("SERR PBQR PNZC");
